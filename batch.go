c2d94f5b29667bf290b1fe1fe5803ecf9f786ef3
Zikun Wang <zikunw@bu.edu>
2024-11-11 15:21:37 -0500

    added batch support

---
+++ batch.go
@@ -0,0 +1,51 @@
+package godata
+
+// A batch contains multiple tuples
+// T must be a tuple type
+type Batch[T Tuple] struct {
+	encode TupleEncoderFunc
+	decode TupleDecoderFunc
+
+	Data []T
+}
+
+// NewBatch creates a new batch of a specific type of tuple
+func NewBatchOf[T Tuple](t T, size uint) Batch[T] {
+	encode := CreateEncoderFunc(t)
+	decode := CreateDecoderFunc(t)
+
+	return Batch[T]{
+		encode: encode,
+		decode: decode,
+		Data:   make([]T, size),
+	}
+}
+
+// Encode a batch into a byte buffer
+func (b *Batch[T]) Encode(buf []byte) int {
+	offset := 0
+	for _, tuple := range b.Data {
+		offset += b.encode(tuple, buf[offset:])
+	}
+	return offset
+}
+
+// Decode a batch from a byte buffer
+// This will read until either
+// 1. The byte buffer is empty OR
+// 2. The buffer is full OR
+// 3. An error has occured during decoding
+//
+// This function will return the number of bytes read
+// from the buffer.
+func (b *Batch[T]) Decode(buf []byte) (int, error) {
+	offset := 0
+	for _, tuple := range b.Data {
+		n, err := b.decode(tuple, buf[offset:])
+		offset += n
+		if err != nil {
+			return offset, err
+		}
+	}
+	return offset, nil
+}
